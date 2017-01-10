b22fe0bba22096deafb06a3ce30a081d459f2dad
real dendii <real dendii>
2017-01-10 09:24:10 +0700

    added file site folder; added text in index file

---
+++ site/assets/readme.txt
@@ -0,0 +1,29 @@
+Details:
+- Index.php  will be the main page of the site 
+- All files used in developing the site will be placed in the ../_assets/ folder
+
+See the ff directories and their content: 
+  
+  1. _assets/css - all css files should be placed here.  Use main.css for the main stylesheet template
+  2. _assets/images -  all images will be placed here.  Use directories to group icons or buttons from 
+      the main images of the site
+  3.  _assets/js - all .js files must be placed here.  
+  4. _assets/vendors - all third-party .js scripts will be placed here.  Ex: JQuery, dojo.js...
+  
+Instructions: 
+In developing CSS:
+1. Used BEM method for naming convention:
+B - stands for Blocks
+E - stands for Element
+M - stands for Modifier
+Search this link for more info: http://getbem.com/introduction/
+2. Used Our Standard Structure of CSS:
+List All Css Accordingly:
+ Tag   (eg. body)
+Class  (eg. .sample)
+ Id    (eg. #sample)
+Arrange the Element/Tag Alphabetically
+Arrange the Ids Alphabetically
+Arrange the Classes Alphabetically
+Arrange the Atrributes Alphabetically
+See css structure in the css folder: main.css
---
+++ site/index.php
@@ -0,0 +1,91 @@
+<!--  
+ - Filename: index.php
+ - Created On: July 8, 2016
+ - Version: 1
+ - Author: Your Name <your.email>
+ - Function: 
+   - This a sample template for every file we make to monitor the authors of our codes 
+   - Ex: This is the main template for the site. 
+-->
+
+<!DOCTYPE html>
+<html lang="">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+	 
+	 <link href = "assets/css/main.css" rel="stylesheet" />
+
+</head>
+
+<body>
+
+<header>
+	<div  class="header__logo-container" ><img class="header__logo" src="assets/images/logo.png"></div>
+		<button>Contact Us</button>
+	<nav>
+	<ul>
+		<li>Home</li>
+		<li>Amenities</li>
+		<li>Features</li>
+		<li>Units</li>
+		<li>360 Walk Through</li>
+	</ul>
+	</nav>
+</header>
+
+	<div class="banner__container"> 
+		<div class="banner__title">
+		<h1>Lorem ipsum dolor sit amet, consectetur</h1>
+		<p>Lorem ipsum dolor sit amet, consectetur adepescing elit. Fusce luctus nec commodo.</p>
+		</div>
+		<div class="banner__subtitle">
+		<p>Lorem ipsum dolor sit amet, consectetur adepescing elit. Fusce luctus nec commodo</p>
+		</div>
+	</div>
+	
+	<div class="container__about-us">
+		<div class= "text__about-us">
+			<h1>About Us</h1>
+			<h3> Lorem Ipsum Dolor Sit Amet</h3>
+		</div>
+		
+		<div class= "text__about-us-sub">
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus justo neque, blandit ut auctor nec, laoreet vel enim. Morbi maximus odio ut tellus feugiat, at lobortis mi ultricies. Fusce sit amet tincidunt tortor, a vestibulum turpis. Aenean et ex non lacus imperdiet efficitur. Maecenas dapibus maximus magna, ut vulputate libero volutpat non. Nulla tempus placerat nulla, at tincidunt enim vulputate ac. </p>
+		
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus justo neque, blandit ut auctor nec, laoreet vel enim. Morbi maximus odio ut tellus feugiat, at lobortis mi ultricies. Fusce sit amet tincidunt tortor, a vestibulum turpis. Aenean et ex non lacus imperdiet efficitur. Maecenas dapibus maximus magna, ut vulputate libero volutpat non. Phasellus ante turpis, facilisis ac justo eget, eleifend lobortis ligula. Nulla tempus placerat nulla, at tincidunt enim vulputate ac. In sit amet velit vel arcu scelerisque venenatis eu sollicitudin risus. Donec vestibulum nunc vel est consectetur, a pellentesque nibh malesuada. Nam efficitur sem suscipit, hendrerit dolor vel, ornare felis. Mauris vestibulum, lorem ut sodales ullamcorper, lorem ante semper enim, interdum pharetra orci augue at nulla. Duis ullamcorper luctus lobortis.s</p>
+			
+			<p>GE EDIT NI</p>
+		</div>
+		</div>
+	   
+	<div class="container__advantage">
+	   <div class="container__advantage-image"> <img class="image__advantage" src="assets/images/circle.jpg"></div>
+		
+		<div class="text__advantage">
+			<h1>The Advantage</h1>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus justo neque, blandit ut auctor nec, laoreet vel enim. Morbi maximus odio ut tellus feugiat, at lobortis mi ultricies. Fusce sit amet tincidunt tortor, a vestibulum turpis. Aenean et ex non lacus imperdiet efficitur. Maecenas dapibus maximus magna, ut vulputate libero volutpat non. Phasellus ante turpis, facilisis ac justo eget, eleifend lobortis ligula. Nulla tempus placerat nulla, at tincidunt enim vulputate ac. In sit amet velit vel arcu scelerisque venenatis eu sollicitudin risus. Donec vestibulum nunc vel est consectetur, a pellentesque nibh malesuada. Nam efficitur sem suscipit, hendrerit dolor vel, ornare felis. Mauris vestibulum, lorem ut sodales ullamcorper, lorem ante semper enim, interdum pharetra orci augue at nulla. Duis ullamcorper luctus lobortis.</p>
+		</div>
+	</div>
+	
+	<div class="container__latest">
+	
+	</div>
+	
+	<div class="container__contact-us">
+	
+	</div>
+	
+	<div class="container__social-media">
+	
+	</div>
+
+
+	<img class="footer__logo" src="assets/images/madrigal.png">
+	<div class="branding__container"><p>&copy; 2015 Grey and Green Technologies, Inc.</p> </div>
+
+	
+
+</body>
+</html>
